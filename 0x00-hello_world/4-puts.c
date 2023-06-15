42b2f77586fb4d8acb36dc747869588b819eace3
taiwo <taiwomuraina19@gmail.com>
2023-06-15 16:56:38 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,10 +3,7 @@
  * main - hello puts project
  * return: o
  */
-echo 'int main(void){
+int main(void){
 	puts("\"Programming is like building a multilingual puzzle"\n);
 	return 0;
-}' > main.c
-gcc main.c -o program 
-./program
-rm main.c program
+}
