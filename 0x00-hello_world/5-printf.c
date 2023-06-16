80c41eaf502f2b150f92f8573634c34d19b63044
taiwo <taiwomuraina19@gmail.com>
2023-06-15 17:30:25 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry for question 5
+ * Return: 0 to indicate success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
