5340b654b2c8fdd0edae2acd586bedcb54db911a
taiwo <taiwomuraina19@gmail.com>
2023-06-15 18:27:16 -0700

    101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	char *me = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	ssize_t len = strlen(me);
-	ssize_t bytes_written = write(STDERR_FILENO, me, len);
+	ssize_t by_length = write(STDERR_FILENO, me, len);
 
-	if (bytes_written != len)
+	if (by_length != len)
 	{
 		write(STDERR_FILENO, "Write error\n", strlen("Write error\n"));
 		return (1);
