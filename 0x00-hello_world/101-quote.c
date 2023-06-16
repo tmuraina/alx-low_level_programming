bffef93ca308d24a60b7aef503be426338a8bf2f
taiwo <taiwomuraina19@gmail.com>
2023-06-15 18:18:39 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+/**
+ * main - Entry for project 101
+ * Return: 1
+ */
+int main(void)
+{
+	char *me = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = strlen(me);
+	ssize_t bytes_written = write(STDERR_FILENO, me, len);
+
+	if (bytes_written != len)
+	{
+		write(STDERR_FILENO, "Write error\n", strlen("Write error\n"));
+		return (1);
+	}
+	return (1);
+}
