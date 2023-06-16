839ca85899f2184b344f588da7e1f6f237a0fd66
taiwo <taiwomuraina19@gmail.com>
2023-06-15 17:18:37 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - Entry point of my hello puts project
- * return: 0 for execution success
+ * Return: 0 for execution success
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\"");
+	puts("\"Programming is like building a multilingual puzzle\");
 	return (0);
 }
