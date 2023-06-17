47ca347a0503d18d57c966a84e7b434b9b99c724
taiwo <taiwomuraina19@gmail.com>
2023-06-16 21:47:23 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry for program 4
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar(num + '0');
+	putchar('\n');
+	return (0);
+}
