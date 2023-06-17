9fedd7fde2331a9b4d44fcd818aef03f2cc8b29d
taiwo <taiwomuraina19@gmail.com>
2023-06-16 21:18:00 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry for program 0
+ * Return: 0 for success
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+
+	putchar('\n');
+
+	return (0);
+}
