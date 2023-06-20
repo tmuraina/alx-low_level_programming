f554ba0005e9df59afcded9b527cff7d2a90952b
taiwo <taiwomuraina19@gmail.com>
2023-06-19 21:42:58 -0700

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,7 +10,7 @@ int main(void)
 
 	for (num1 = 0; num1 <= 99; num1++)
 	{
-		for (num2 = num1; num2 <= 99 && num2 >= num1; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
 			putchar(num1 / 10 + '0');
 			/* To print the tens digit */
@@ -27,5 +27,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return (0);
+	return 0;
 }
