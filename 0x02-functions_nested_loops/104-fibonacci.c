eeb49861f595a4a6a232e3fde333bd79be08e66e
taiwo <taiwomuraina19@gmail.com>
2023-06-20 10:58:37 -0700

    .

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,20 +9,36 @@
 int main(void)
 {
 	unsigned long int i;
-	unsigned long int prev1 = 1;
-	unsigned long int prev2 = 2;
+	unsigned long int prev = 1;
+	unsigned long int next = 2;
+	unsigned long int x = 1000000000;
+	unsigned long int prev1;
+	unsigned long int prev2;
+	unsigned long int next1;
+	unsigned long int next2;
 
-	printf("%lu", prev1);
+	printf("%lu", prev);
 
-	for (int i = 1; i < 98; i++)
+	for (i = 1; i < 91; i++)
 	{
-		printf(", %lu", prev2);
-		prev2 += prev1;
-		prev1 = prev2 - prev1;
+		printf(", %lu", next);
+		next += prev;
+		prev = next - prev;
 	}
+	prev1 = (prev / x);
+	prev2 = (prev % x);
+	next1 = (next / x);
+	next2 = (next % x);
 
-
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", next1 + (next2 / x));
+		printf("%lu", next2 % x);
+		next1 = next1 + prev1;
+		prev1 = next1 - prev1;
+		next2 = next2 + prev2;
+		prev2 = next2 - prev2;
+	}
 	printf("\n");
-
 	return (0);
 }
