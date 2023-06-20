849d9521fcef62fe22eb9dec656e52393e49c0c2
taiwo <taiwomuraina19@gmail.com>
2023-06-20 04:57:08 -0700

    .

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,23 +24,13 @@ void print_times_table(int n)
 				_putchar(' ');
 
 				if (result < 10)
-				{
 					_putchar(' ');
+				if (result < 100)
 					_putchar(' ');
-					_putchar(result + '0');
-				}
-				else if (result < 100)
-				{
-					_putchar(' ');
-					_putchar((result / 10) + '0');
-					_putchar((result % 10) + '0');
-				}
-				else
-				{
-					_putchar((result / 100) + '0');
-					_putchar(((result / 10) % 10) + '0');
-					_putchar((result % 10) + '0');
-				}
+
+				_putchar((result / 100) + '0');
+				_putchar(((result / 10) % 10) + '0');
+				_putchar((result % 10) + '0');
 			}
 			else
 			{
