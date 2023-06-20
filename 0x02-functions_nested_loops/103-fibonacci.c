735299db0b220f7553ca07e29dbab76da99b9687
taiwo <taiwomuraina19@gmail.com>
2023-06-20 10:33:54 -0700

    .

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,60 +1,32 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
  * main - Entry for task 103
  * Return: always return 0
- * prev: previous term
- * current: current term
  * next: next term
  * sum: initial sum of even_valued terms
  */
 
 int main(void)
 {
-	long int prev = 1;
-	long int current = 2;
-	long int next;
-	long int sum = 2;
+	int i;
+	unsigned long int j, k, next, sum;
 
-	while (current <= 4000000)
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 1; j <= 4000000; ++i)
 	{
-		next = prev + current;
-		if (next % 2 == 0)
-			sum += next;
-		prev = current;
-		current = next;
+		if (j % 2 == 0)
+		{
+			sum += j;
+		}
+		next = j + k;
+		j = k;
+		k = next;
 	}
-
-	print_number(sum);
-	_putchar('\n');
+	printf("%lu\n", sum);
 
 	return (0);
-}
-
-/**
- * print_number - prints a number
- * @num: the number
- */
-
-void print_number(long int num)
-{
-	if (num == 0)
-	{
-		_putchar('0');
-		return;
-	}
-
-	char buffer[20];
-	int i = 0;
-
-	while (num != 0)
-	{
-		buffer[i] = (num % 10) + '0';
-		num /= 10;
-		i++;
-	}
-
-	for (int j = i - 1; j >= 0; j--)
-		_putchar(buffer[j]);
 }
