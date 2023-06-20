a5ba8029bf8a00694ed07ae89fd55a10dacbb63d
taiwo <taiwomuraina19@gmail.com>
2023-06-20 03:59:13 -0700

    .

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,7 +1,13 @@
 #include <unistd.h>
 
+/**
+ * _putchar - writees c to the output
+ * @c: the character
+ * Return: 1 for success, -1 otherwise
+ */
+
 int _putchar(char c)
 {
-    return write(1, &c, 1);
+    return (write(1, &c, 1));
 }
 
