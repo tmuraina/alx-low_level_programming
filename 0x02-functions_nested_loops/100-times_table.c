a75d3e509569e7adf17e9895b3bdacb8961e5b58
taiwo <taiwomuraina19@gmail.com>
2023-06-20 08:17:09 -0700

    .

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,34 +9,41 @@ void print_times_table(int n)
 {
 	int i, j, result;
 
-	if (n > 15 || n < 0)
-		return;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (i = 0; i <= n; i++)
 		{
-			result = i * j;
-
-			if (j != 0)
+			for (j = 0; j <= n; j++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				result = i * j;
 
-				if (result < 10)
+				if (j != 0)
+				{
+					_putchar(result + '0');
+				} else if (result < 10 && j != 0)
+				{
+					_putchar(',');
 					_putchar(' ');
-				if (result < 100)
 					_putchar(' ');
-
-				_putchar((result / 100) + '0');
-				_putchar(((result / 10) % 10) + '0');
-				_putchar((result % 10) + '0');
+					_putchar(' ');
+					_putchar(result + '0');
+				} else if (result >= 10 && result < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((result / 10) + '0');
+					_putchar((result % 10) + '0');
+				} else if (result >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((result / 100) + '0');
+					_putchar(((result / 10) % 10) + '0');
+					_putchar((result % 10) + '0');
+				}
 			}
-			else
-			{
-				_putchar('0');
-			}
+			_putchar('\n')
 		}
-		_putchar('\n');
 	}
 }
